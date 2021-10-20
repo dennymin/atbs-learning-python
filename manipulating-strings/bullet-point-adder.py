9a6c6701f64e4be18be51c57f9303012463ea126
Denny Min <denden223@gmail.com>
2021-10-20 15:48:47 -0700

    Finished a project that makes bulleted lists. - Denny Min

---
+++ manipulating-strings/bullet-point-adder.py
@@ -0,0 +1,14 @@
+#1 python3
+# This adds Wikipedia bullet points to the start of each line of text on the clipboard
+
+import pyperclip
+
+text = pyperclip.paste()
+
+# Separate lines and add stars
+lines = text.split('\n')
+for i in range(len(lines)):
+  lines[i] = '* ' + lines[i]
+text = '\n'.join(lines)
+
+pyperclip.copy(text)
